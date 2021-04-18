c16fe21c6d92cff3d0b9d6d7b0ce2c52f1faecd0
Jacob Chalk <jc17360@bristol.ac.uk>
2021-04-18 15:37:04 +0100

    FaissKNN

--- Scripts/sampling.py
+++ Scripts/sampling.py
@@ -1,12 +1,25 @@
 import pandas as pd
 import numpy as np
+import faiss
 import time
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import NearestNeighbors
 from random import randrange, uniform
-from collections import Counter
-from numpy import where
+
+class FaissKNeighbors:
+    def __init__(self, n_neighbors=5):
+        self.index = None
+        self.y = None
+        self.n_neighbors = n_neighbors
+
+    def fit(self, X, y):
+        self.index = faiss.IndexFlatL2(X.shape[1])
+        self.index.add(X.astype(np.float32))
+        self.y = y
+
+    def kneighbors(self, X):
+        return self.index.search(X.astype(np.float32), k=self.n_neighbors)
 
 def SMUTE(X_maj, y_maj, n_smute):
     X_maj_prime = X_maj.copy().reset_index(drop=True)
@@ -14,8 +27,11 @@ def SMUTE(X_maj, y_maj, n_smute):
 
     x3_y = y_maj_prime.sample().copy()
 
-    knn = NearestNeighbors(n_neighbors=6)
+    knn = FaissKNeighbors(n_neighbors=6)
 
+    last_print = 0
+
+    print(" KNN FIT - SAMPLING")
     while((len(X_maj) - len(X_maj_prime)) < n_smute):
         x1_id = X_maj_prime.sample().index.item()
         x1 = X_maj_prime.iloc[x1_id]
@@ -33,6 +49,11 @@ def SMUTE(X_maj, y_maj, n_smute):
         X_maj_prime = X_maj_prime.append(x3, ignore_index=True)
         y_maj_prime = y_maj_prime.append(x3_y, ignore_index=True)
 
+        completion = (len(X_maj) - len(X_maj_prime)) / n_smute
+        if completion > last_print:
+            print(f"     {completion:.3f}%")
+            last_print += 0.001
+
     return X_maj_prime, y_maj_prime
 
 def SMOTE(X_min, y_min, n_smote):
@@ -44,9 +65,14 @@ def SMOTE(X_min, y_min, n_smote):
 
     x3_y = y_min_prime.sample().copy()
 
-    knn = NearestNeighbors(n_neighbors=6)
-    knn.fit(X_min.to_numpy(), y_min.to_numpy())
+    knn = FaissKNeighbors(n_neighbors=6)
+    X_numpy = X_min.to_numpy().copy(order='C')
+    y_numpy = y_min.to_numpy().copy(order='C')
+    knn.fit(X_numpy, y_numpy)
 
+    last_print = 0
+
+    print(" KNN FIT - SAMPLING")
     while((len(X_min_prime) - len(X_min)) < n_smote):
         x1_id = X_min.sample().index.item()
         x1 = X_min.iloc[x1_id]
@@ -58,6 +84,11 @@ def SMOTE(X_min, y_min, n_smote):
         x3 = x1 + (r * (x2 - x1))
         X_min_prime = X_min_prime.append(x3, ignore_index=True)
         y_min_prime = y_min_prime.append(x3_y, ignore_index=True)
+
+        completion = (len(X_min_prime) - len(X_min)) / n_smote
+        if completion > last_print:
+            print(f"     {completion:.3f}%")
+            last_print += 0.001
 
     return X_min_prime, y_min_prime
 
@@ -72,11 +103,14 @@ def CSMOUTE(majority, minority, ratio):
     n_smote = round(n * ratio)
     n_smute = n - n_smote
 
+    print("SMOTE")
     X_min_prime, y_min_prime = SMOTE(X_min, y_min, n_smote)
+    print("SMUTE")
     X_maj_prime, y_maj_prime = SMUTE(X_maj, y_maj, n_smute)
 
     return X_maj_prime, y_maj_prime, X_min_prime, y_min_prime
 
+print("LOADING DATA")
 meta_data = pd.read_csv("data/meta_features_data.csv", index_col=0)
 
 non_fraud = meta_data[meta_data['is_fraud'] == 0]
@@ -88,3 +122,6 @@ non_fraud_samples = samples[0]
 non_fraud_samples['is_fraud'] = samples[1]
 fraud_samples = samples[2]
 fraud_samples['is_fraud'] = samples[3]
+
+non_fraud_samples.to_csv("data/non_fraud_samples.csv")
+fraud_samples.to_csv("data/fraud_samples.csv")
