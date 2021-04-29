ebf53529cbe67c4a9b0f27504fd63e779cf2d71b
Jacob Chalk <jc17360@bristol.ac.uk>
2021-04-29 14:30:08 +0100

    Sampling scripts

--- Scripts/SMOTE.py
+++ Scripts/SMOTE.py
@@ -7,6 +7,7 @@ meta_data = pd.read_csv("data/meta_features_train.csv", index_col=0)
 y = meta_data["is_fraud"]
 X = meta_data.drop(["is_fraud"], axis=1)
 
+print("SAMPLING")
 oversample = imblearn.over_sampling.SMOTE(sampling_strategy=1.0)
 
 X, y = oversample.fit_resample(X, y)
