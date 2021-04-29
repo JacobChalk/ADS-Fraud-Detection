3058ec8bd88927031f4703a3b3b5ccab01a76c7f
Jacob Chalk <43956457+JacobChalk@users.noreply.github.com>
2021-04-29 14:33:56 +0100

    Update bc4_template.sh

--- Scripts/bc4_template.sh
+++ Scripts/bc4_template.sh
@@ -9,8 +9,8 @@
 # Change these parameters above to suit the script. Typically --time and --mem affect the priority queue the most
 # And BC4 prioritises jobs that request more nodes E.g. BC4 configs (viewed with "scontrol show config") has set "PriorityFavorSmall = No"
 # To specify a specific amount of memory, add "#SBATCH --mem X(MB|GB)". Use only MB or GB, not (MB|GB). Lower means less queue time. However, leaving this
-# Flag blank means you have access to ~100MB memory on the node. Useful if you don't know how much memory your program will consume and don't want it to error.
-# When you submit, if you type squeue --user <user> and see "QOSMinCpuNotSatisfied" then you need to up either nodes, ntasks-per-node or cpus-per-task
+# Flag blank means you have access to ~100MB memory on the node. When you submit, if you type squeue --user <user> and 
+# See "QOSMinCpuNotSatisfied" then you need to increase either nodes, ntasks-per-node or cpus-per-task
 
 # Get rid of any modules already loaded
 module purge
